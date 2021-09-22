e9c90e15dae492cb41d3c2f7c63f910ef1417554
songor <chensongyu1028@gmail.com>
2021-09-22 23:42:07 +0800

    条件和循环

---
+++ Head First Go/guess.go
@@ -0,0 +1,49 @@
+// guess challenges players to guess a random number.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	seconds := time.Now().Unix()
+	rand.Seed(seconds)
+	target := rand.Intn(100) + 1
+	fmt.Println("I've chosen a random number between 1 and 100.")
+	fmt.Println("Can you guess it?")
+
+	reader := bufio.NewReader(os.Stdin)
+	success := false
+	for guesses := 0; guesses < 10; guesses++ {
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if guess < target {
+			fmt.Println("Oops. Your guess was LOW.")
+		} else if guess > target {
+			fmt.Println("Oops. Your guess was HIGH.")
+		} else {
+			success = true
+			break
+		}
+	}
+	if success {
+		fmt.Println("Good job! You guessed it!")
+	} else {
+		fmt.Println("Sorry, you didn't guess my number. It was:", target)
+	}
+}
