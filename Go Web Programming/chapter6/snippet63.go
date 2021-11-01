134a94d37c3763f91f1f6ef0ab016818517cbda4
songor <chensongyu1028@gmail.com>
2021-11-01 23:49:35 +0800

    第六章 存储数据

---
+++ Go Web Programming/chapter6/snippet63.go
@@ -0,0 +1,63 @@
+// 读写 CSV 文件
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type CsvPost struct {
+	Id      int
+	Content string
+	Author  string
+}
+
+func main() {
+	csvFile, err := os.Create("posts.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer csvFile.Close()
+
+	allPosts := []CsvPost{
+		{Id: 1, Content: "Hello World!", Author: "Sau Sheong"},
+		{Id: 2, Content: "Bonjour Monde!", Author: "Pierre"},
+		{Id: 3, Content: "Hola Mundo!", Author: "Pedro"},
+		{Id: 4, Content: "Greetings Earthlings!", Author: "Sau Sheong"},
+	}
+
+	writer := csv.NewWriter(csvFile)
+	for _, post := range allPosts {
+		line := []string{strconv.Itoa(post.Id), post.Content, post.Author}
+		err = writer.Write(line)
+		if err != nil {
+			panic(err)
+		}
+	}
+	writer.Flush()
+
+	file, err := os.Open("posts.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	reader.FieldsPerRecord = -1
+	records, err := reader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	var posts []CsvPost
+	for _, item := range records {
+		id, _ := strconv.ParseInt(item[0], 0, 0)
+		post := CsvPost{Id: int(id), Content: item[1], Author: item[2]}
+		posts = append(posts, post)
+	}
+	fmt.Println(posts[0].Id)
+	fmt.Println(posts[0].Content)
+	fmt.Println(posts[0].Author)
+}
