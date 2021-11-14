91c316498a3213f66a058fbf3d28903b76efd78c
songor <chensongyu1028@gmail.com>
2021-11-14 17:51:33 +0800

    第六章 存储数据

---
+++ Go Web Programming/chapter6/snippet614.go
@@ -0,0 +1,81 @@
+// 使用 Go 语言实现一对多以及多对一关系
+package main
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	_ "github.com/lib/pq"
+)
+
+type MapPost struct {
+	Id       int
+	Content  string
+	Author   string
+	Comments []MapComment
+}
+
+type MapComment struct {
+	Id      int
+	Content string
+	Author  string
+	Post    *MapPost
+}
+
+var MapDb *sql.DB
+
+func init() {
+	var err error
+	MapDb, err = sql.Open("postgres", "user=gwp dbname=gwp password=gwp.123 sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (comment *MapComment) Create() (err error) {
+	if comment.Post == nil {
+		err = errors.New("Post not found")
+		return
+	}
+	MapDb.QueryRow("insert into comments (content, author, post_id) values ($1, $2, $3) returning id", comment.Content, comment.Author, comment.Post.Id).Scan(&comment.Id)
+	return
+}
+
+func GetPostAndComments(id int) (post MapPost, err error) {
+	post = MapPost{}
+	post.Comments = []MapComment{}
+	err = MapDb.QueryRow("select id, content, author from posts where id = $1", id).Scan(&post.Id, &post.Content, &post.Author)
+
+	rows, err := MapDb.Query("select id, content, author from comments where post_id = $1", id)
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		comment := MapComment{Post: &post}
+		err = rows.Scan(&comment.Id, &comment.Content, &comment.Author)
+		if err != nil {
+			return
+		}
+		post.Comments = append(post.Comments, comment)
+	}
+	rows.Close()
+	return
+}
+
+func (post *MapPost) Create() (err error) {
+	err = MapDb.QueryRow("insert into posts (content, author) values ($1, $2) returning id", post.Content, post.Author).Scan(&post.Id)
+	return
+}
+
+func main() {
+	post := MapPost{Content: "Hello World!", Author: "Sau Sheong"}
+	post.Create()
+
+	comment := MapComment{Content: "Good post!", Author: "Joe", Post: &post}
+	comment.Create()
+
+	readPost, _ := GetPostAndComments(post.Id)
+	fmt.Println(readPost)
+	fmt.Println(readPost.Comments)
+	fmt.Println(readPost.Comments[0].Post)
+}
---
+++ Go Web Programming/chapter6/snippet66.go
@@ -0,0 +1,88 @@
+// 使用 Go 对 Postgres 执行 CRUD 操作
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+)
+
+type DbPost struct {
+	Id      int
+	Content string
+	Author  string
+}
+
+var Db *sql.DB
+
+func init() {
+	var err error
+	Db, err = sql.Open("postgres", "user=gwp dbname=gwp password=gwp.123 sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Posts(limit int) (posts []DbPost, err error) {
+	rows, err := Db.Query("select id, content, author from posts limit $1", limit)
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		post := DbPost{}
+		err = rows.Scan(&post.Id, &post.Content, &post.Author)
+		if err != nil {
+			return
+		}
+		posts = append(posts, post)
+	}
+	rows.Close()
+	return
+}
+
+func GetPost(id int) (post DbPost, err error) {
+	post = DbPost{}
+	err = Db.QueryRow("select id, content, author from posts where id = $1", id).Scan(&post.Id, &post.Content, &post.Author)
+	return
+}
+
+func (post *DbPost) Create() (err error) {
+	statement := "insert into posts (content, author) values ($1, $2) returning id"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	err = stmt.QueryRow(post.Content, post.Author).Scan(&post.Id)
+	return
+}
+
+func (post *DbPost) Update() (err error) {
+	_, err = Db.Exec("update posts set content = $2, author = $3 where id = $1", post.Id, post.Content, post.Author)
+	return
+}
+
+func (post *DbPost) Delete() (err error) {
+	_, err = Db.Exec("delete from posts where id = $1", post.Id)
+	return
+}
+
+func main() {
+	post := DbPost{Content: "Hello World!", Author: "Sau Sheong"}
+
+	fmt.Println(post)
+	post.Create()
+	fmt.Println(post)
+
+	readPost, _ := GetPost(post.Id)
+	fmt.Println(readPost)
+
+	readPost.Content = "Bonjour Monde!"
+	readPost.Author = "Pierre"
+	readPost.Update()
+
+	posts, _ := Posts(5)
+	fmt.Println(posts)
+
+	readPost.Delete()
+}
